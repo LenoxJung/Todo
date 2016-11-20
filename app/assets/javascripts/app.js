d6e8d6db39e9ca79adb956413be2540c556fa657
Lenny <lenoxjung@gmail.com>
2016-11-19 20:46:40 -0800

    finished display

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
 
+  function addTodo(todo) {
+    $('#todos').append('<li>' + todo.title + '</li>');
+  }
+
   $('#form').on('submit', function(event){
+    event.preventDefault();
     var todo = $('#todo').val();
     $.ajax({
       url: '/todos',
       method: 'POST',
       datatype: 'json',
       data: { title: todo },
-      success: $('#todo').val('')
+      success: addTodo
     });
-    event.preventDefault();
+    
+  });
+
+  $.getJSON('/todos', function(todos) {
+    $.each(todos, function() {
+      addTodo(this);
+    });
   });
 
 });
